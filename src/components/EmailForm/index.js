f36ed39e00e9a4212e2d2f3aa44defdb9ae44a20
glyif <122@holbertonschool.com>
2017-11-09 19:49:28 -0800

    updating styling for email form

--- src/components/EmailForm/index.js
+++ src/components/EmailForm/index.js
@@ -2,12 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Input, Button, withStyles } from 'material-ui';
 
-const styles = {
+const styles = theme => ({
     container: {
         maxWidth: '1280px',
         margin: '0 auto',
     },
-};
+    inputRoot: {
+        padding: 0,
+        '&:before': {
+            content: 'none',
+        },
+        '&:after': {
+            content: 'none',
+        },
+    },
+    input: {
+        border: 'none',
+        borderRadius: 4,
+        background: 'rgba(255, 255, 255, 0.12)',
+        color: theme.palette.common.white,
+        fontSize: 16,
+        padding: '10px 15px',
+        transition: 'background-color .2s ease',
+        '&:focus': {
+            background: 'rgba(255, 255, 255, 0.2)',
+        },
+    },
+    button: {
+        backgroundColor: '#68c0f8',
+        borderRadius: 8,
+    },
+});
 
 const EmailForm = ({ classes }) => (
     <Grid
@@ -17,11 +42,17 @@ const EmailForm = ({ classes }) => (
         <Grid item>
             <Input
                 placeholder="Enter your email"
-                className={classes.input}
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.input,
+                }}
             />
         </Grid>
         <Grid item>
-            <Button type="primary">
+            <Button
+                color="contrast"
+                className={classes.button}
+            >
                 Get Early Access
             </Button>
         </Grid>
