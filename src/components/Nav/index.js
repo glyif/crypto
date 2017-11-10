70f8171dbfdc740fa5e466aaae271faaf342af9e
glyif <122@holbertonschool.com>
2017-11-09 20:01:51 -0800

    nav bar styling changes

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as R from 'ramda';
-import { withStyles } from 'material-ui';
+import { Button, withStyles } from 'material-ui';
 
-const styles = {
+const styles = theme => ({
     root: {
         display: 'flex',
         flexGrow: 1,
-        alignItems: 'center',
+        justifyContent: 'center',
     },
     navItem: {
         display: 'flex',
         // TODO: use variables
         padding: '10px 15px 15px',
+        textTransform: 'none',
+        fontSize: theme.typography.body1.fontSize,
+        fontWeight: 'normal',
+        color: theme.palette.common.white,
+        '&:hover': {
+            background: 'none',
+        },
     },
-};
+});
 
 const Nav = ({ classes, items }) => (
     <div className={classes.root}>
         {R.map(({ label, url }) => (
-            <a
-                key={url}
-                className={classes.navItem}
+            <Button
+                key={label}
+                classes={{ root: classes.navItem }}
                 href={url}
+                component="a"
+                dense
             >
                 {label}
-            </a>
+            </Button>
         ), items)}
     </div>
 );
